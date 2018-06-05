37bca08cea9a9511eadba6ac184fa4d05356348a
Tekk <marc.wilhelm.ch@gmail.com>
2018-06-05 10:08:43 +0200

    Added draggable numbers as ship placeholders

--- templates/Game/js/run.js
+++ templates/Game/js/run.js
@@ -8,3 +8,17 @@ $(async () => {
     }
     await startup.loadGame(username, sessionKey);
 });
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    const data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
