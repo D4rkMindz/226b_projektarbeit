2191e399e1fd80abeeabf271ae0001711a65c70a
Björn Pfoster <bjoern.pfoster@gmail.com>
2018-06-25 22:29:43 +0200

    Updated setup script

--- bin/setup-websocket.sh
+++ bin/setup-websocket.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #kill current websocket job
-nohup php /home/d4rkmindz/schoolproject/htdocs/htdocs/bin/game-server.php 0 &> /dev/null &
+nohup php /home/d4rkmindz/schoolproject/htdocs/htdocs/bin/game-server.php 0 &
 # restart job
 disown
 
