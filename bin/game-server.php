9f8556fa5c403b313fc485870d360813e30642a8
Björn <bjoern.pfoster@gmail.co>
2018-06-26 09:33:54 +0200

    Test

--- bin/game-server.php
+++ bin/game-server.php
@@ -11,14 +11,14 @@ require_once __DIR__ . '/../config/bootstrap.php';
 
 // Keep script running
 // https://stackoverflow.com/questions/9798438/automatically-restart-php-script-on-exit
-if (function_exists('pcntl_exec')) {
-    echo ++$argv[1];
-    $_ = $_SERVER['_'];
-    register_shutdown_function(function () {
-        global $_, $argv;
-        pcntl_exec($_, $argv);
-    });
-}
+//if (function_exists('pcntl_exec')) {
+//    echo ++$argv[1];
+//    $_ = $_SERVER['_'];
+//    register_shutdown_function(function () {
+//        global $_, $argv;
+//        pcntl_exec($_, $argv);
+//    });
+//}
 
 $actionhandler = new ActionHandler();
 $observer = new Observer();
